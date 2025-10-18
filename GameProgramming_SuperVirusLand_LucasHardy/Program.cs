e81b5f3f960caf088e81483683578cc66dea8cb8
Lucas Hardy <lucashardywork@gmail.com>
2025-10-18 16:40:19 -0300

    completed code

--- GameProgramming_SuperVirusLand_LucasHardy/Program.cs
+++ GameProgramming_SuperVirusLand_LucasHardy/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Security.Cryptography.X509Certificates;
 using System.Runtime.InteropServices;
+using System.Reflection;
 
 namespace GameProgramming_SuperVirusLand_LucasHardy
 {
@@ -17,8 +18,15 @@ namespace GameProgramming_SuperVirusLand_LucasHardy
         static Random random = new Random(); 
         static Random randomVirusSpawn = new Random();
 
-        
 
+        static List<(int, int)> virusList = new List<(int, int)>();
+
+
+        static int x;
+        static int y;
+
+        static int newX;
+        static int newY;
 
         static char[,] mapArray =
             {
@@ -37,47 +45,23 @@ namespace GameProgramming_SuperVirusLand_LucasHardy
 
         static void Main(string[] args)
         {
-            
-            
-
-            List<(int, int)> virusList = new List<(int, int)>();
             virusList.Add((4, 0));
             virusList.Add((7, 1));
             virusList.Add((1, 7));
 
-            
 
-            
-            
-            
+
+
+
+
+
+
+
             while (gameOver == false)
             {
+
+                virusMovement();
                 
-
-                for (int i = 0; i < virusList.Count; i++)
-                {
-                    int x = virusList[i].Item1;
-                    int y = virusList[i].Item2;
-
-                    int newX = (x + random.Next(-1, 2));
-                    int newY = (y + random.Next(-1, 2));
-
-                    if (newX >= mapArray.GetLength(1) || newY >= mapArray.GetLength(0) || newX < 0 || newY < 0)
-                    {
-                        virusList[i] = (x, y);
-                    }
-                    else
-                    {
-                        virusList[i] = (newX, newY);
-                        
-                    }
-
-                    
-
-
-                    
-
-                }
 
 
                 for (int y = 0; y < mapArray.GetLength(0); y++)
@@ -103,17 +87,73 @@ namespace GameProgramming_SuperVirusLand_LucasHardy
                 }
                 Thread.Sleep(1000);
                 Console.Clear();
+                virusReproduction();
+
             }
 
         }
-        static void virusMovement()
+        static void virusReproduction()
         {
             
+
+            for (int i = 0; i < virusList.Count; i++)
+            {
+                int Copy = random.Next(0, 10);
+
+                if (Copy == 5)
+                {
+                    virusList.Add(virusList[i]);
+                }
+            }
+                
 
 
 
 
         }
-       
+
+        static void virusMovement()
+        {
+            for (int i = 0; i < virusList.Count; i++)
+            {
+
+                x = virusList[i].Item1;
+                y = virusList[i].Item2;
+
+                newX = (x + random.Next(-1, 2));
+                newY = (y + random.Next(-1, 2));
+
+                if(newX == x)
+                {
+                    virusList[i] = (x, newY);
+                }
+                else if (newY == y)
+                {
+                    virusList[i] = (newX, y);
+                }
+
+
+                if (newX >= mapArray.GetLength(1) || newY >= mapArray.GetLength(0) || newX < 0 || newY < 0)
+                {
+                    virusList[i] = (x, y);
+                }
+
+                else if  (mapArray[newY, newX] == '^' || mapArray[newY, newX] == '~')
+                {
+                    virusList[i] = (x, y);
+                }
+                
+                else if (virusList[i] == (newX, newY))
+                {
+
+                    
+
+                }
+                
+
+
+            }
+        }
+
     }
 }
